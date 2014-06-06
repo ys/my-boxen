7e22eaa2e890b135b3fcfa11bb69ae6ad2bf999f
Yannick <yannick.schutz@gmail.com>
2014-06-06 19:00:26 +0200

    Bundler for all

--- modules/people/manifests/ys/rubies.pp
+++ modules/people/manifests/ys/rubies.pp
@@ -5,4 +5,9 @@ class people::ys::rubies {
 
   ruby::version { '2.1.1': }
   ruby::version { '2.0.0-p451': }
+  ruby_gem { 'bundler for all rubies':
+    gem          => 'bundler',
+    version      => '~> 1.6.2',
+    ruby_version => '*',
+  }
 }
